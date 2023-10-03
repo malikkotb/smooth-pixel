b97c813590b711029c8c99b42122169f48eed711
Malik Kotb <malikkotb@icloud.com>
2023-10-03 22:48:35 +0200

    structure

---
+++ app/page.js
@@ -0,0 +1,10 @@
+import Menu from "@/components/menu/menu";
+export default function Home() {
+  return (
+    <>
+      <div className="">
+        <Menu />
+      </div>
+    </>
+  );
+}
---
+++ components/menu/menu.js
@@ -0,0 +1,6 @@
+import { FiMenu } from "react-icons/fi"
+export default function menu() {
+  return (
+    <div><FiMenu className="h-12 w-12" /></div>
+  )
+}
